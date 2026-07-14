90fe77ab811205ad9df7128f64789393ce8d0647
renatossjxd <107287804+renatossjxd@users.noreply.github.com>
2026-07-14 17:29:53 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
